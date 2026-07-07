d55104d2f5ee9174afcd50679fab29dab72b7e63
rbanyalswd-art <rbanyalswd@gmail.com>
2026-07-07 22:56:26 +0300

    Update pairing number and owner contact format

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212666666666;
+global.pairingNumber = 966 54 852 3941;
 global.owner = [
-  ['212717457920', 'noureddine Ouafy', true],
+  ['966 54 852 3941', 'noureddine Ouafy', true],
   ['', 'Owner 2', true],
 ];
 
